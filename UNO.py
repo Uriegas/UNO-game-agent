6eebac5861678d8dd7d64575e2d38342d84b2c5f
1930526@upv.edu.mx <1930526@upv.edu.mx>
2022-01-19 12:44:33 -0600

    Metaconfiguration

--- UNO.py
+++ UNO.py
@@ -1,21 +1,29 @@
 from UnoGame import *
 
-## Globals
-colors = [ Color.red, Color.blue, Color.green, Color.yellow ]
-specials = [Special.blockNextPlayer, Special.plusTwo, Special.reverse]
-cards = []
+## In this file we define the configuration of the game:
+## * How many cards there are?
+## * How many players there are?
+## * How many player bots there are?
+
+## This configuration is static but can be done dinamically with further programming
+
+## Current game configuration
+colors = [ Color.red, Color.blue, Color.green, Color.yellow ] ## Card colors
+specials = [Special.blockNextPlayer, Special.plusTwo, Special.reverse] ## Special powers
+numbers = range(0, 10) # Numbers of the cards
+players = [("Maquina de Fuego", True), ("Eduardo", False)] ## List with name and isBot of players
+cards = [] ## Array of cards
 
 ## ---------------- Initialize cards array ----------------
 ## Cards from 0 to 9, 4 colors
-for number in range(0, 9):
+for number in numbers:
     for color in colors:
         cards.append(UnoCard(number, color, None))
 
 ## Cards from 1 to 9, 4 colors
-for number in range(1, 9):
+for number in numbers[1:]:
     for color in colors:
         cards.append(UnoCard(number, color, None))
-
 
 ## Special Cards, 3 special cards 2 times each color = 3 x 2 x 4 = 24 cards
 for special in specials:
@@ -30,6 +38,5 @@ for color in colors:
 
 
 ## ---------------- Start UNO game ----------------
-game = UnoGame(cards, playersNames=["Maquina de Fuego", "Eduardo"], cardsPerPlayer=8)
+game = UnoGame(cards, playersNames=players, cardsPerPlayer=8)
 game.start()
-# print(game)
