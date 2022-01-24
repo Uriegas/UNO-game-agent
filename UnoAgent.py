79f47369d533a11e6a3129b59a5e7687ff45abf1
1930526@upv.edu.mx <1930526@upv.edu.mx>
2022-01-23 21:41:14 -0600

    Board and special cards done, need to do agents logic

--- UnoAgent.py
+++ UnoAgent.py
@@ -1,14 +1,16 @@
 from typing import List, Tuple
-
+from xmlrpc.client import Boolean
 from UnoCard import UnoCard
 import random
 
-
 class UnoAgent:
-    def __init__(self, id: int, name: str, cards: List ) -> None:
+    def __init__(self, id: int, name: str, isBot: Boolean, cards: List ) -> None:
         self.id = id
         self.name = name
+        self.isBot = isBot
         self.cards = cards
+    def setName(self, name: str) -> None:
+        self.name = name
     def getCards(self) -> List:
         return self.cards
     def addCards(self, cards: List) -> None:
@@ -23,10 +25,49 @@ class UnoAgent:
         return self.name
     def __str__(self) -> str:
         string = self.name + "{\n\tid: " + str(self.id) + ",\n\tcards{\n"
-        for card in self.cards:
-            string += '\t\t' + str(card) + '\n'
+        if self.cards != None:
+            for card in self.cards: ## TODO: Change to indexed for loop
+                string += '\t\t' + str(card) + '\n'
         return string + "\t}\n}"
-    def move(self, currentCard: UnoCard, deck: List) -> Tuple:
+
+    def action(self, cardStack: List, actionsQueue: List = None) -> UnoCard:
+        if cardStack != []:
+            currentCard = cardStack[-1]
+        else:
+            currentCard = None
+        ## Returns selected card or None when there is a withdraw from deck request
+        if self.isBot is False: ## If not a bot display prompt
+            ## TODO: Make a better validation of the input
+            while True:
+                print(self) ## Print the players cards
+                print("Current card is: " + str(currentCard))
+                # print("Previous actions: ")
+                # for action in actionsQueue:
+                #     print("\t" + str(action))
+                try:
+                    while True: ## Loop for invalid **number** input
+                        movement = input("Insert the number of your selection (Type '?' for help)> ")
+                        selectedCard = self.getCardAt(int(movement)) # If this fails, except will be raised
+                        if selectedCard == currentCard: ## Validate card can be selected. Remember __eq__ is implemented
+                            return selectedCard
+                        else: # TODO: Need to move validation of movement to a parser, otherwise I need to use a goto here
+                            print("Invalid move, try again!")
+                except: ## if a **string** is inputted
+                    if(movement == 'g'): # Get card from deck
+                        return None ## Bad practice, but it works, so passing None means get card from deck
+                    elif(movement == '?'):
+                        print("Type on of the following options")
+                        for i, card in zip( range(len(self.getCards())), self.getCards() ):
+                            print(str(i) + ". " + str(card))
+                        print("g. For getting a card from the deck")
+                        print("?. For help (You are already seeing this)")
+                        input("Press any key to continue >") ## Change this for keyboard Listener https://stackoverflow.com/questions/48787563/press-esc-to-stop-and-any-other-key-to-continue-in-python#48960318
+                    else:
+                        print("What the heck brah, try again!")
+        else:
+            return self.move(cardStack)
+
+    def move(self, cardStack: List) -> UnoCard:
         '''Consider that the agent could be modeled in such a way that
            it makes inferences about what cards other players have'''
         # Get set of possible actions
@@ -36,20 +77,17 @@ class UnoAgent:
             ## If card is of the same color or same number or is special it is eligable
             if currentCard == self.cards[i]:
                 actions.append(i)
-        
         ## Note that in this selection process we do self.cards.append(card) when we select
         ## a card, in order to just pop it from the cards list at the end of the function.
         if actions != None: ## If there are actions that can be performed
             indexSelectedCard = random.choice(actions) ## Select random action
             self.cards.append(self.cards.pop(indexSelectedCard))
-        else: ## Since there are not possible actions keep eating cards until match
+        else: ## Since there are not possible actions eat card
             newCard = deck.pop()
             self.cards.append(newCard)
-
             while currentCard != newCard:
                 newCard = deck.pop()
                 self.cards.append(newCard)
-
         # Select a card according to the card
         # Remove that card from the list and return it
-        return self.cards.pop(), deck
+        return self.cards.pop()
--- UnoGame.py
+++ UnoGame.py
@@ -1,23 +1,23 @@
 from typing import List
+
 from UnoAgent import UnoAgent
-from UnoCard import UnoCard
 from Utilities import *;
 import random
 
 class UnoGame:
-    def __init__(self, deck, playersNames: List, cardsPerPlayer: int) -> None:
+    def __init__(self, deck, playersConfig: List, cardsPerPlayer: int) -> None:
         self.deck = deck # The bunch of cards in the game
-        self.players = []
-        self.currentCard = None
-        self.winner = None
+        self.players = [] # List of players in the game
+        # self.currentCard = None # Deprecated
+        self.winner = None # Set to None if there is no winner yet
+        self.cardStack = [] # Stack of cards that had been played
 
         random.shuffle(self.deck) # Shuffle cards
 
         # Set players cards
         # TODO: Program fails if cardsPerPlayer or numberPlayers are so big
-        for name in playersNames:
-            self.players.append( UnoAgent(id=random.randint(1, 1000), name=name, cards=[self.deck.pop() for i in range(cardsPerPlayer)]) )
-
+        for config in playersConfig:
+            self.players.append( UnoAgent(id=random.randint(1, 1000), name=config[0], isBot=config[1], cards=[self.deck.pop() for i in range(cardsPerPlayer)]) )
 
     def start(self) -> None:
         print("           )     )   ")
@@ -28,43 +28,62 @@ class UnoGame:
         print("| | | || \| | / _ \  Welcome to UNO GAME from terminal.")
         print("| |_| || .` || (_) | You are playing against a machine.")
         print(" \___/ |_|\_| \___/\n")
+        while self.winner is None:
+            print(self.players[0]) # Verbose
+            self.play() ## In special cards we change the players array inside the board method call.
+            self.players.append( self.players.pop(0) ) ## This is the change of player turn
+        self.printWinner()
 
-        while( self.winner is None ):
-            # Print environment
-            for player in self.players:
-                print(player)
-            print("Current card is: " + str(self.currentCard))
-            movement = input("Your turn (Type '?' for help)> ")
-            try:
-                selectedCard = self.players[i].getCardAt(int(movement))
-                if selectedCard == self.currentCard: ## Validate card can be selected
-                    self.currentCard = selectedCard
-                else: # TODO: Need to move validation of movement to a parser, otherwise I need to use a goto here
-                    print("Your movement is not valid, please make a valid one.")
-            except:
-                if(movement == 'g'): # Get card from deck
-                    self.players[1].addCard(self.deck.pop())
-                elif(movement == '?'):
-                    print("Type on of the following options")
-                    for i, card in zip( range(len(self.players[1].getCards())), self.players[1].getCards() ):
-                        print(str(i) + ". " + str(card))
-                    print("g. For getting a card from the deck")
-                    print("?. For help (You are already seeing this)")
-                    input("Press any key to continue >") ## Change this and add a keyboard Listener https://stackoverflow.com/questions/48787563/press-esc-to-stop-and-any-other-key-to-continue-in-python#48960318
-                else:
-                    print("Invalid move, try again!")
-            
-            # Machine plays
-            self.currentCard, self.deck = self.players[0].move(self.currentCard, self.deck)
+    def play(self) -> None:
+        ## The player plays
+        player = self.players[0] ## This creates a pointer not a copy
+        # An action is performed by the given player
+        card = player.action(self.cardStack) ## TODO: Add more parameters to this function
+        if card is not None:
+            self.cardStack.append(card) ## Add card to the stack
+            if card.special is not None: ## Is a special action
+                if card.special == Special.plusTwo:
+                    self.requestAdd(2)
+                elif card.special == Special.plusFour:
+                    self.colorChangePrompt()
+                    self.requestAdd(4)
+                elif card.special == Special.colorChange:
+                    self.colorChangePrompt()
+                elif card.special == Special.reverse:
+                    tmp = self.players[1:]
+                    tmp.reverse()
+                    self.players = [self.players[0]] + tmp
+                elif card.special == Special.blockNextPlayer:
+                    self.players.append( self.players.pop(0) ) # As if the next player had played
+        else:## Is a card request petition
+            newCard = self.deck.pop()
+            print( player.name + " your new card is: " + str(newCard) )
+            player.addCard(newCard)
 
-            for player in self.players:
-                if(len(player.getCards()) == 0):
-                    self.winner = player
-                    break
-        
-        print(self.winner.getName() + " wins!")
+
+    def printWinner(self) -> None:
+        print("The winner is: " + self.winner.name)
         print("Chika chika yeah!")
-
+    
+    def colorChangePrompt(self) -> None:
+        ## The color of the special card changes to the selected one
+        currentCard = self.cardStack[-1]
+        while True: ## Ugliest code ever
+            selection = input("Choose new color (red, green, blue, yellow)> ")
+            if selection == "red":
+                currentCard.setColor( Color.red )
+                break
+            elif selection == "green":
+                currentCard.setColor( Color.green )
+                break
+            elif selection == "blue":
+                currentCard.setColor( Color.blue )
+                break
+            elif selection == "yellow":
+                currentCard.setColor( Color.yellow )
+                break
+            else:
+                print("Invalid color, please type a valid one.")
     
     def __str__(self) -> str:
         string = "UNO GAME\n"
@@ -75,3 +94,13 @@ class UnoGame:
         for player in self.players:
             string += str(player) + '\n'
         return string
+    
+    ## NEED TO TEST THIS CODE
+    def requestAdd(self, number: int) -> None:
+        ## The current player requests to add a number of cards to the next player
+        nextPlayer = self.players[1] ## Guaranteed that there at list 2 players
+        nextPlayer.addCard(self.deck.pop() for i in range(number))
+        ## This is faster than using pop()
+        # selectedCards = self.deck[-number:]
+        # del self.deck[-number:]
+        # nextPlayer.addCards(selectedCards)
