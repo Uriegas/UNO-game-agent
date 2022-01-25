b2f8b6256ece6ea0c4f285a1afcc25755173987d
1930526@upv.edu.mx <1930526@upv.edu.mx>
2022-01-24 20:24:19 -0600

    Dummy agent

--- UnoAgent.py
+++ UnoAgent.py
@@ -3,12 +3,17 @@ from xmlrpc.client import Boolean
 from UnoCard import UnoCard
 import random
 
+DIFFICULTY = 1 # Hardcoded difficulty
+
 class UnoAgent:
     def __init__(self, id: int, name: str, isBot: Boolean, cards: List ) -> None:
         self.id = id
         self.name = name
         self.isBot = isBot
         self.cards = cards
+        if(isBot):
+            self.difficulty = DIFFICULTY
+        # self.state ## Bot state
     def setName(self, name: str) -> None:
         self.name = name
     def getCards(self) -> List:
@@ -17,10 +22,6 @@ class UnoAgent:
         self.cards.append(cards)
     def addCard(self, card: UnoCard) -> None:
         self.cards.append(card)
-    def getCardAt(self, index: int) -> UnoCard:
-        return self.cards[index]
-    def removeCardAt(self, index: int) -> UnoCard:
-        return self.cards.pop(index)
     def getName(self) -> str:
         return self.name
     def __str__(self) -> str:
@@ -36,10 +37,10 @@ class UnoAgent:
         else:
             currentCard = None
         ## Returns selected card or None when there is a withdraw from deck request
+        print(self) ## Print the players cards
         if self.isBot is False: ## If not a bot display prompt
             ## TODO: Make a better validation of the input
             while True:
-                print(self) ## Print the players cards
                 print("Current card is: " + str(currentCard))
                 # print("Previous actions: ")
                 # for action in actionsQueue:
@@ -47,9 +48,10 @@ class UnoAgent:
                 try:
                     while True: ## Loop for invalid **number** input
                         movement = input("Insert the number of your selection (Type '?' for help)> ")
-                        selectedCard = self.getCardAt(int(movement)) # If this fails, except will be raised
+                        index = int(movement)
+                        selectedCard = self.cards[index] # If this fails, except will be raised
                         if selectedCard == currentCard: ## Validate card can be selected. Remember __eq__ is implemented
-                            return selectedCard
+                            return self.cards.pop(index)
                         else: # TODO: Need to move validation of movement to a parser, otherwise I need to use a goto here
                             print("Invalid move, try again!")
                 except: ## if a **string** is inputted
@@ -68,26 +70,57 @@ class UnoAgent:
             return self.move(cardStack)
 
     def move(self, cardStack: List) -> UnoCard:
-        '''Consider that the agent could be modeled in such a way that
-           it makes inferences about what cards other players have'''
-        # Get set of possible actions
-        actions = [] ## Indexes of cards that can be selected to perform the action
-        selectedAction  = None
-        for i in range(len(self.cards)): ## Search for cards that meet the action criteria
-            ## If card is of the same color or same number or is special it is eligable
-            if currentCard == self.cards[i]:
+        '''
+        Consider that the agent could be modeled in such a way that
+        it makes inferences about what cards other players have
+        State is not necesary because the agent is always playing when executing this method
+        '''
+        seq = cardStack ## Sequence of cards
+        actions = self.getActions(cardStack[-1], self.cards) ## Actions of the agent: list of indexes
+        if actions != []: ## If there are actions that can be performed
+            return self.cards.pop(self.getAction(actions)) ## Return the selected card
+        else: ## Since there are no possible actions eat a card
+            return None
+    
+    def getActions(self, currentCard: UnoCard, cards: List) -> List:
+        '''
+        Returns a list of indexes of cards that can be selected to perform the action
+        '''
+        actions = []
+        for i in range(len(cards)):
+            if currentCard == cards[i]:
                 actions.append(i)
-        ## Note that in this selection process we do self.cards.append(card) when we select
-        ## a card, in order to just pop it from the cards list at the end of the function.
-        if actions != None: ## If there are actions that can be performed
-            indexSelectedCard = random.choice(actions) ## Select random action
-            self.cards.append(self.cards.pop(indexSelectedCard))
-        else: ## Since there are not possible actions eat card
-            newCard = deck.pop()
-            self.cards.append(newCard)
-            while currentCard != newCard:
-                newCard = deck.pop()
-                self.cards.append(newCard)
-        # Select a card according to the card
-        # Remove that card from the list and return it
-        return self.cards.pop()
+        return actions
+    
+    def getAction(self, actions: List) -> int:
+        '''
+        Returns the index of the selected card
+        '''
+        if self.difficulty == 0:
+            return random.choice(actions)
+        elif self.difficulty == 1:
+            return self.getActionMedium(actions)
+        elif self.difficulty == 2:
+            return self.getActionHard(actions)
+        else:
+            print("Invalid difficulty")
+            return None
+    
+    def getActionMedium(self, actions: List) -> int:
+        '''
+        Returns the index of the selected card
+        '''
+        if len(actions) == 1:
+            return actions[0]
+        else:
+            return random.choice(actions)
+    
+    def selectColor(self, cardStack: List) -> str:
+        '''
+        Returns the color of the selected card
+
+        Note: At the moment this function only handles if self.isBot is True
+        but in the future it should handle both cases and the function should return a Color
+        selectColor(self, cardStack: List) -> Color
+        '''
+        return random.choice(["red", "blue", "green", "yellow"])
