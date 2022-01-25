d4bc9a7ecbb192b1340d3200b6aa972532f02839
1930526@upv.edu.mx <1930526@upv.edu.mx>
2022-01-24 20:22:03 -0600

    Fix card validation

--- UnoCard.py
+++ UnoCard.py
@@ -10,7 +10,7 @@ class UnoCard:
     def __eq__(self, __o: object) -> bool:
         ## If card is of the same color or same number or is special or is None is eligable
         ## TODO: Check this logic later
-        if (__o == None) or (__o.color != None and __o.color == self.color) or (__o.number != None and __o.number == self.number) or (__o.special != None):
+        if (__o is None) or (__o.color is not None and __o.color == self.color) or (__o.number is not None and __o.number == self.number) or (__o.special is not None and __o.special == self.special) or (self.special == Special.colorChange or self.special == Special.plusFour):
             return True
         return False
     def __str__(self) -> str:
